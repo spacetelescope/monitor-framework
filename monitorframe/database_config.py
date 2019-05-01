3e00ade701a0f78b6d4703fd7e5e08ff55465cfc
James White <jwhite@stsci.edu>
2019-05-01 10:32:08 -0400

    Remove examples

--- monitorframe/database_config.py
+++ monitorframe/database_config.py
@@ -1,9 +1,3 @@
 SETTINGS = {
-    'database': '/Users/jwhite/monitoring/Monitors.db',
-    'pragmas': {
-        'journal_mode': 'wal',
-        'foreign_keys': 1,
-        'ignore_check_constraints': 0,
-        'synchronous': 0
-    }
+    'database': '',
 }
