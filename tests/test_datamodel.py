ef20aaca019e6e8ce0788d08031fb25f4119508d
James White <jwhite@stsci.edu>
2019-07-31 17:03:07 -0400

    Add comments and docstrings to tests

--- tests/test_datamodel.py
+++ tests/test_datamodel.py
@@ -27,7 +27,13 @@ NEW_DATA_WITH_NPARRAY = {
 
 @pytest.fixture(params=[NEW_DATA, NEW_DATA_WITH_LIST, NEW_DATA_WITH_NPARRAY])
 def datamodel_test_instance(request):
+    """Test fixture that creates a datamodel object (from BaseDataModel) using the different data configurations in
+    NEW_DATA, NEW_DATA_WITH_LIST, and NEW_DATA_WITH_NPARRAY. This fixture also includes a clean-up if a database table
+    is created.
 
+    Note: request is a special pytest argument.
+    """
+    # Create a simple implementation of a DataModel class for testing
     class DataModelTestObject(BaseDataModel):
         primary_key = 'a'
 
@@ -36,18 +42,22 @@ def datamodel_test_instance(request):
 
     datamodel_test_instance = DataModelTestObject()
 
+    # yield the instance
     yield datamodel_test_instance
 
+    # Post-test cleanup
     if datamodel_test_instance.model:
         datamodel_test_instance.model.drop_table()
 
 
 class TestDataModel:
-
+    """Test class for testing the monitorframe BaseDataModel."""
     def test_new_data_is_dataframe(self, datamodel_test_instance):
+        """Test that the result of get_new_data is successfully wrapped and returns a pandas DataFrame."""
         assert isinstance(datamodel_test_instance.new_data, pd.DataFrame)
 
     def test_find_array_types(self, datamodel_test_instance):
+        """Test that the array column is found successfully."""
         test_array_keys = ['c']
 
         if datamodel_test_instance._array_types:
@@ -57,6 +67,7 @@ class TestDataModel:
             assert not datamodel_test_instance._array_types
 
     def test_ingest_format(self, datamodel_test_instance):
+        """Test that for array columns, the elements are converted into strings."""
         test_array_keys = ['c']
 
         if datamodel_test_instance._array_types:
@@ -72,39 +83,50 @@ class TestDataModel:
             assert all(datamodel_test_instance.new_data == datamodel_test_instance._formatted_data)
 
     def test_ingest(self, datamodel_test_instance):
+        """Test that the ingest method executes successfully."""
         datamodel_test_instance.ingest()
 
     def test_model_creation(self, datamodel_test_instance):
-        datamodel_test_instance.ingest()
-        query = list(datamodel_test_instance.model.select().dicts())
+        """Test that the peewee model object is created successfully (post-ingestion)."""
+        datamodel_test_instance.ingest()  # The model is created via introspection. There must be a table with data.
 
+        # Check that a peewee model can be constructed and queried
+        query = list(datamodel_test_instance.model.select().dicts())
         assert len(query) == 3
 
+        # Check that the model columns are correct
+        assert list(datamodel_test_instance.model._meta.columns.keys()) == ['a', 'b', 'c']
+
     def test_ingest_fails(self, datamodel_test_instance):
+        """Test that the table does not accept duplicates when a primary key is defined."""
         datamodel_test_instance.ingest()
 
         with pytest.raises(IntegrityError):
             datamodel_test_instance.ingest()
 
     def test_db_is_closed(self, datamodel_test_instance):
+        """Test that the database connection closes after the ingest method executes successfully."""
         datamodel_test_instance.ingest()
-
         assert datamodel_test_instance._database.is_closed() is True
 
     def test_model_returns_none_without_table(self, datamodel_test_instance):
+        """Test that the model property is None if the table doesn't exist."""
         assert datamodel_test_instance.model is None
 
     def test_query_to_pandas(self, datamodel_test_instance):
+        """Test the query to pandas method."""
         datamodel_test_instance.ingest()
-
         query = datamodel_test_instance.model.select()
 
         if datamodel_test_instance._array_types:
+            # Check that the converted columns are floats by default
             df = datamodel_test_instance.query_to_pandas(query, ['c'])
             assert (type(df.c[0]) == list or type(df.c[0]) == np.ndarray) and type(df.c[0][0]) == np.float64
 
+            # Check that the columns are converted into the specified dtype successfully
             df = datamodel_test_instance.query_to_pandas(query, ['c'], [int])
             assert (type(df.c[0]) == list or type(df.c[0]) == np.ndarray) and type(df.c[0][0]) == np.int64
 
         else:
+            # Check that the query is converted without array elements
             datamodel_test_instance.query_to_pandas(query)
--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -27,7 +27,7 @@ SIMPLE_IMAGE = {'x': 'b', 'y': 'c', 'z': 'd', 'plottype': 'image'}
 
 @pytest.fixture
 def datamodel_test_instance():
-
+    """Test fixture for a DataModel object for use in testing BaseMonitor."""
     class DataModelTestObject(BaseDataModel):
         primary_key = 'a'
 
@@ -41,7 +41,12 @@ def datamodel_test_instance():
     params=[NOTIFICATIONS, OUTPUT_DIR, OUTPUT_PATH, NAME, SUBPLOTS, LABELS, SIMPLE_SCATTER, SIMPLE_LINE, SIMPLE_IMAGE]
 )
 def monitor_test_instance(datamodel_test_instance, request):
+    """Test fixture for a Monitor object for use in testing. This fixture is parametrized with different attribute
+    configurations that affect Monitor behavior.
 
+    Note: request is a special pytest argument.
+    """
+    # Create a simple implementation of the BaseMonitor
     class MonitorTestObject(BaseMonitor):
         data_model = datamodel_test_instance
         notification_settings = request.param.get('notification_settings', None)
@@ -69,23 +74,27 @@ def monitor_test_instance(datamodel_test_instance, request):
 
     monitor_test_instance = MonitorTestObject()
 
+    # Yield that Monitor object
     yield monitor_test_instance
 
-    if monitor_test_instance.model.model:
+    # Post-test cleanup
+    if monitor_test_instance.model.model:  # Drop any data tables created during testing
         monitor_test_instance.model.model.drop_table()
 
-    if monitor_test_instance.results_table is not None:
+    if monitor_test_instance.results_table is not None:  # Drop any results tables created during testing
         monitor_test_instance._table.drop_table()
 
-    if os.path.exists(monitor_test_instance.output):
+    if os.path.exists(monitor_test_instance.output):  # Remove output files created during testing.
         os.remove(monitor_test_instance.output)
 
 
 class TestMonitor:
-
+    """Test class for testing the monitorframe BaseMonitor."""
     def test_monior_init(self, monitor_test_instance):
-        assert monitor_test_instance.model.new_data is not None
+        """Test that attributes are created or set correctly for each configuration."""
+        assert monitor_test_instance.model.new_data is not None  # New data should be retrieved by default
 
+        # Check that a table object is created and that the name is set correctly
         assert (
                 monitor_test_instance._table is not None and
                 monitor_test_instance._table._meta.table_name == monitor_test_instance.__class__.__name__
@@ -94,25 +103,29 @@ class TestMonitor:
         assert monitor_test_instance.datetime_col is not None
         assert monitor_test_instance.result_col is not None
 
+        # Check that if the suplots and subplots_layout are set, that the figure is created with multiple axes
         if monitor_test_instance.subplots:
             assert 'xaxis2' in monitor_test_instance.figure.layout
 
+        # Check that the name attribute is set correctly
         if (
-                monitor_test_instance.name !=
+                monitor_test_instance.name !=  # default name
                 monitor_test_instance.__class__.__name__ + f': {monitor_test_instance.date.date().isoformat()}'
         ):
             assert (
-                    monitor_test_instance.name ==
+                    monitor_test_instance.name ==  # specified name
                     'Test Monitor Name' + f': {monitor_test_instance.date.date().isoformat()}'
             )
 
+        # Check that the filename attribute is created correctly.
         assert monitor_test_instance._filename == '_'.join(monitor_test_instance.name.split(': ')).replace(' ', '')
 
     def test_set_mailer(self, monitor_test_instance):
+        """Test that the mailer object is created successfully"""
         if monitor_test_instance.notification_settings is not None:
             monitor_test_instance.initialize_data()
-            monitor_test_instance.run_analysis()
-            monitor_test_instance._set_mailer()
+            monitor_test_instance.run_analysis()  # set_mailer is called here
+
             assert monitor_test_instance.mailer is not None
             assert monitor_test_instance.mailer.sender == 'testuser@stsci.edu'
             assert monitor_test_instance.mailer.subject == monitor_test_instance.name
@@ -120,68 +133,85 @@ class TestMonitor:
             assert monitor_test_instance.mailer.recipients == 'testrecipient@stsci.edu'
 
     def test_results_table(self, monitor_test_instance):
+        """Test that the results table property is set correctly."""
+        # The table doesn't exist, so the query object shouldn't exist
         assert monitor_test_instance.results_table is None
 
         monitor_test_instance.store_results()
-
         assert monitor_test_instance.results_table is not None
 
     def test_initialize_data(self, monitor_test_instance):
+        """Test the initialize_data method."""
         monitor_test_instance.initialize_data()
 
-        assert monitor_test_instance.data is not None
+        assert monitor_test_instance.data is not None  # data attribute should be set
 
-        if monitor_test_instance.labels is not None:
+        if monitor_test_instance.labels is not None:  # hover_text column should be created
             assert 'hover_text' in monitor_test_instance.data
 
     def test_run_analysis(self, monitor_test_instance):
+        """Test run_analysis method."""
         monitor_test_instance.initialize_data()
         monitor_test_instance.run_analysis()
 
-        assert monitor_test_instance.results is not None
+        assert monitor_test_instance.results is not None  # results attribute should be set
+
+        # outliers attribute should also be set since find_outliers was defined
         assert monitor_test_instance.outliers is not None
 
+        # Check notifications are set if the test instance is configured with notification settings
         if monitor_test_instance.notification_settings is not None:
             assert monitor_test_instance.notification is not None
             assert monitor_test_instance.mailer is not None
 
     def test_plot(self, monitor_test_instance):
+        """Test that the plot method executes successfully and has the correct number of traces."""
+        # Check for test instances that are configured with x and y set
         if monitor_test_instance.x is not None and monitor_test_instance.y is not None:
-            monitor_test_instance.initialize_data()
-            monitor_test_instance.run_analysis()
+            monitor_test_instance.initialize_data()  # set data attributes
+            monitor_test_instance.run_analysis()  # set optional outliers attribute
             monitor_test_instance.plot()
 
-            if monitor_test_instance.plottype == 'scatter':
+            if monitor_test_instance.plottype == 'scatter':  # Outliers are not plotted for line or image plots
                 assert len(monitor_test_instance.figure.data) == 2 if monitor_test_instance.plottype == 'scatter' else 1
 
     def test_write_figure(self, monitor_test_instance):
+        """Test that the figure is written a file successfully"""
         if monitor_test_instance.x is not None and monitor_test_instance.y is not None:
             monitor_test_instance.initialize_data()
             monitor_test_instance.plot()
-            monitor_test_instance.write_figure()
 
+            # Plot and check that the file is created at the output attribute path
+            monitor_test_instance.write_figure()
             assert os.path.exists(monitor_test_instance.output)
 
     def test_monitor(self, monitor_test_instance):
+        """Test that the monitor method executes successfully"""
+        # Exclude email configurations for now
         if monitor_test_instance.notification_settings is None:
             monitor_test_instance.monitor()
 
+            # Check that get_data is executed
             assert monitor_test_instance.data is not None
 
+            # Check that the hover labels are created
             if monitor_test_instance.labels is not None:
                 assert 'hover_text' in monitor_test_instance.data
 
-            assert monitor_test_instance.results is not None
-            assert monitor_test_instance.outliers is not None
-            assert os.path.exists(monitor_test_instance.output)
-            assert monitor_test_instance.results_table is not None
+            assert monitor_test_instance.results is not None  # Check that track is executed
+            assert monitor_test_instance.outliers is not None  # Check that find_outliers is executed
+            assert os.path.exists(monitor_test_instance.output)  # Check that plot is executed
+            assert monitor_test_instance.results_table is not None  # Check that store_results is executed
 
     def test_define_hover_labels(self, monitor_test_instance):
-        monitor_test_instance.initialize_data()
+        """Test that the hover labels are defined correctly (column name    value)."""
+        monitor_test_instance.initialize_data()  # Create hover labels
 
+        # Check for the configuration where the labels are set
         if monitor_test_instance.labels is not None:
             assert monitor_test_instance.data.hover_text[0] == 'a    A'
 
     def test_init_basemonitor_fails(self):
+        """Test that BaseMonitor can't be used directly."""
         with pytest.raises(TypeError):
             BaseMonitor()
