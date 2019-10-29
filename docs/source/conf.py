6a9492c4cad593c5c85271299d460f5b379d9e2e
James White <jwhite@stsci.edu>
2019-10-29 09:38:27 -0400

    Increment version number.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, James White'
 author = 'James White'
 
 # The short X.Y version
-version = ''
+version = '1.1.1'
 # The full version, including alpha/beta/rc tags
-release = ''
+release = '1.1.1'
 
 
 # -- General configuration ---------------------------------------------------
