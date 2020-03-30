c457af6484d51f5333c7cc68166c25bd9106a3f4
James White <jwhite@stsci.edu>
2020-03-30 16:25:07 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup_parameters = dict(
 
 setup(
     name='monitorframe',  # Required
-    version='1.2.0',  # Required
+    version='1.2.1',  # Required
     description='Framework for building instrument monitors.',  # Required
     author='James White; Space Telescope Science Institute',
     author_email='jwhite@stsci.edu',
